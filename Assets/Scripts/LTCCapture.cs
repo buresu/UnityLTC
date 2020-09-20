81b8a5a93c3310c868cc8b66c09c66925edf3d9c
Naoto Kondo <cgigcp3yqt@gmail.com>
2020-09-21 05:35:13 +0900

    Audio input

---
+++ Assets/Scripts/LTCCapture.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LTCCapture : MonoBehaviour
+{
+    // Audio
+    private string _device = "";
+    private AudioClip _audioInput = null;
+
+    private void Start()
+    {
+        // List devices
+        foreach (var d in Microphone.devices)
+            Debug.Log(d);
+
+        // Get device
+        _device = Microphone.devices[6];
+
+        // Start capture
+        _audioInput = Microphone.Start(_device, true, 1, 44100);
+        Debug.Log("Capture " + _device);
+    }
+
+    private void Stop()
+    {
+        // Stop capture
+        Microphone.End(_device);
+    }
+
+    private void Update()
+    {
+
+    }
+}
