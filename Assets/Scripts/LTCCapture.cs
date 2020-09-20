94f87fa6339fb6b0fb3e824f874de25db78439e4
Naoto Kondo <cgigcp3yqt@gmail.com>
2020-09-21 05:41:50 +0900

    Capture audio buffer

--- Assets/Scripts/LTCCapture.cs
+++ Assets/Scripts/LTCCapture.cs
@@ -7,6 +7,8 @@ public class LTCCapture : MonoBehaviour
     // Audio
     private string _device = "";
     private AudioClip _audioInput = null;
+    private List<float> _audioBuffer = new List<float>();
+    private int _lastAudioPos = 0;
 
     private void Start()
     {
@@ -30,6 +32,28 @@ public class LTCCapture : MonoBehaviour
 
     private void Update()
     {
+        // Update audio buffer
+        int pos = Microphone.GetPosition(_device);
 
+        int size1 = pos < _lastAudioPos ? _audioInput.samples * _audioInput.channels - _lastAudioPos : 0;
+        int size2 = pos >= _lastAudioPos ? pos - _lastAudioPos : pos;
+
+        if (size1 > 0)
+        {
+            var data = new float[size1];
+            _audioInput.GetData(data, _lastAudioPos);
+            _audioBuffer.AddRange(new List<float>(data));
+        }
+
+        if (size2 > 0)
+        {
+            var data = new float[size2];
+            _audioInput.GetData(data, 0);
+            _audioBuffer.AddRange(new List<float>(data));
+        }
+
+        _lastAudioPos = pos;
+
+        //Debug.Log(_audioBuffer.Count);
     }
 }
